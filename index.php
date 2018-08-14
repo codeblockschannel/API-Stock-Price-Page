0b7ccd60c953e645714c9ed0ddcd136a47ae18c6
codeblockschannel <42357552+codeblockschannel@users.noreply.github.com>
2018-08-13 22:50:14 -0400

    Update index.php

--- index.php
+++ index.php
@@ -10,14 +10,17 @@
 <body>
 	<table class="stocks">
 		<?php
+			//runs get.php script when the page first loads
 			require_once('get.php');
 		?>
 	</table>
 	<script>
+		//calls the get.php script every 3000 milliseconds (3 seconds)
 		$(function() {
 			setInterval(function() {
 				$.get("get.php", function(update) {
 					$('.stocks').html(update);
+					//replaces the contents of the "stocks" class table with the results from get.php
 				});
 			}, 3000);
 		});
